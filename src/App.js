dc42aae8d450c3aa7ac1898e37e3973e0985d014
ingridmoitinho <ingridmoitinho@gmail.com>
2024-12-23 00:53:21 -0300

    módulo 3: interagindo com o usuário

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 
+import { useState } from 'react';
 import Banner from './components/Banner';
-import CampoTexto from './components/CampoTexto';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+   setColaboradores([...colaboradores, colaborador]);
+  };
+
   return (
     <div className="App">
       <Banner /> 
-      <Formulario /> 
-    
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>     
     </div>
   );
 }
