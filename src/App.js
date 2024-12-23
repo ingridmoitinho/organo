54660e9a51eb039b2a89dbd321a696c040fb8b03
ingridmoitinho <ingridmoitinho@gmail.com>
2024-12-22 22:18:54 -0300

    Commit módulo 1: uma biblioteca declarativa

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Banner from './components/Banner';
+import CampoTexto from './components/CampoTexto';
+import Formulario from './components/Formulario';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner /> 
+      <Formulario /> 
+    
+    </div>
+  );
+}
+
+export default App;
